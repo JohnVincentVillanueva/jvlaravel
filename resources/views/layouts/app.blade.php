dbae366d0af57b143b60ad87a4064ce51dff47db
john vincent villanueva <johnvincent2408@gmail.com>
2024-05-26 01:57:21 +0800

    Nice Admin Successfully Integrated!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,23 +30,14 @@
          <link href="assets/css/style.css" rel="stylesheet">
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+        @include('layouts.header')
+         @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <!-- Page Content -->
+        <main id="main" class="main">
+            {{ $slot }}
+         </main>
+         @include('layouts.footer')
         <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
         <!-- Vendor JS Files -->
