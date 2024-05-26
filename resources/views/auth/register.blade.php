790cee02c7edc82dc9d076ce65044bfb2e71e628
john vincent villanueva <johnvincent2408@gmail.com>
2024-05-27 07:08:12 +0800

    Breeze successfuly New  Installed!

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" class="row g-3 needs-validation">
         @csrf
 
         <!-- Name -->
@@ -12,13 +12,13 @@
         <!-- Email Address -->
         <div class="col-12">
             <x-input-label for="email" :value="__('Email')" class="form-label" />
-            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="invalid-feedback" />
         </div>
 
         <!-- Password -->
         <div class="col-12">
-            <x-input-label for="password" :value="__('Password')" class="form-label"/>
+            <x-input-label for="password" :value="__('Password')" class="form-label" />
 
             <x-text-input id="password" class="form-control"
                             type="password"
@@ -30,7 +30,7 @@
 
         <!-- Confirm Password -->
         <div class="col-12">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="form-label"/>
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="form-label" />
 
             <x-text-input id="password_confirmation" class="form-control"
                             type="password"
@@ -39,17 +39,14 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="invalid-feedback" />
         </div>
 
-        <br>
-
-        <!-- Register btn -->
-        <x-primary-button class="btn btn-primary w-100">
-                {{ __('Register') }}
-            </x-primary-button> 
-
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
+
+            <x-primary-button class="btn btn-primary w-100">
+                {{ __('Register') }}
+            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
